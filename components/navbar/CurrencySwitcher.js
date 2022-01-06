ca1661eefaec1b93b0d7951872020c6f75872ec7
jomapps <jomapps.jb@gmail.com>
2022-01-07 02:01:24 +0800

    cleanup

--- components/navbar/CurrencySwitcher.js
+++ components/navbar/CurrencySwitcher.js
@@ -8,28 +8,29 @@ import {Fragment, useContext} from "react";
 import AppWideContext from "../../store/AppWideContext";
 
 
-function CurrencySwitcher(props){
+function CurrencySwitcher(props) {
   const mobileView = null;
-  const {dataStore,updateDataStore} = useContext(AppWideContext);
+  const {dataStore, updateDataStore} = useContext(AppWideContext);
   const currCurrency = dataStore.currCurrency;
   const currencies = appSettings("currencies");
   const currencyData = appSettings("currency_data");
 
   let options = null;
-  currencies.forEach(currency=>{
+  currencies.forEach(currency => {
     const currencySymbol = currencyData[currency].curr_symbol;
     options = <Fragment>
       {options}
-      <option value={currency} {(currCurrency==currency)?'selected="selected"':null}>
+      <option value={currency}>
         {currencySymbol}{currency.toUpperCase()}
       </option>
     </Fragment>;
   })
 
   const browserView =
-    <select id="currency-switcher" onChange={(event)=>updateDataStore("currCurrency", event.target.value)}>
+    <select id="currency-switcher" value={currCurrency}
+            onChange={(event) => updateDataStore("currCurrency", event.target.value)}>
       {options}
-  </select>;
+    </select>;
 
   return (
     (props.isMobile) ? mobileView : browserView
