557a248979a86d717791f8d66a1ad9447cc3ad28
jomapps <jomapps.jb@gmail.com>
2022-01-06 21:21:14 +0800

    Added component WhySalt

---
+++ components/why-salt/WhySalt.js
@@ -0,0 +1,17 @@
+/**
+ * @params {isMobile} props
+ * @constructor
+ */
+
+function WhySalt(props){
+  const mobileView = null;
+
+  const browserView = null;
+
+  return (
+    (props.isMobile) ? mobileView : browserView
+  )
+
+}
+
+export default WhySalt;
