9bbfaf56c4eaa3f5ddae056973ba6ebd544da6a2
Sambhav399 <sambhavsharma1999@gmail.com>
2022-01-07 10:24:49 +0530

    Navbar Completed

--- components/navbar/Menu.js
+++ components/navbar/Menu.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import SubMenu from "./SubMenu";
+import React from 'react';
 
 /**
  *
@@ -8,31 +9,84 @@ import SubMenu from "./SubMenu";
  */
 
 function Menu(props) {
-  const mobileView = null;
 
-  const browserView =
-    <div>
-      <ul>
-        <li><Link href="/new-arrivals/all"><a>New In</a></Link></li>
-        <li><Link href="/looks"><a>Looks</a></Link></li>
-        <li><Link href="/shop-tops"><a>Tops</a></Link></li>
-        <li><Link href="/shop-sweaters"><a>Sweaters</a></Link></li>
-        <li><Link href="/shop-shirts"><a>Shirts</a></Link></li>
-        <li><Link href="/shop-tunics"><a>Tunics</a></Link></li>
-        <li><Link href="/shop-jumpsuits"><a>Jumpsuits</a></Link></li>
-        <li><Link href="/shop-shorts"><a>Shorts</a></Link></li>
-        <li><Link href="/shop-tailored-pants"><a>Pants</a></Link></li>
-        <li><Link href="/shop-tailored-skirts"><a>Skirts</a></Link></li>
-        <li><Link href="/shop-dresses"><a>Dresses</a></Link></li>
-        <li><Link href="/shop-outerwear"><a>Outerwear</a></Link></li>
-        <li><SubMenu isMobile={false} menu="accessories" /></li>
-      </ul>
-    </div>
-  ;
+    const browserViewStyle = "block px-4 py-1 mx-1 leading-none border-b border-transparent hover:border-black";
 
-  return (
-    (props.isMobile) ? mobileView : browserView
-  )
+    const mobileView = null;
+    const browserView =
+        <>
+            <ul className={"flex flex-1 justify-center"}>
+                <li>
+                    <Link href="/new-arrivals/all">
+                        <a className={browserViewStyle + " bg-[#B5DDF5] text-white"}>New In</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/looks">
+                        <a className={browserViewStyle}>Looks</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-tops">
+                        <a className={browserViewStyle}>Tops</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-sweaters">
+                        <a className={browserViewStyle}>Sweaters</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-shirts">
+                        <a className={browserViewStyle}>Shirts</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-tunics">
+                        <a className={browserViewStyle}>Tunics</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-jumpsuits">
+                        <a className={browserViewStyle}>Jumpsuits</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-shorts">
+                        <a className={browserViewStyle}>Shorts</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-tailored-pants">
+                        <a className={browserViewStyle}>Pants</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-tailored-skirts">
+                        <a className={browserViewStyle}>Skirts</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-dresses">
+                        <a className={browserViewStyle}>Dresses</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/shop-outerwear">
+                        <a className={browserViewStyle}>Outerwear</a>
+                    </Link>
+                </li>
+                <li className={"relative group"}>
+                    <span className={browserViewStyle + " group-hover:border-black"}>Accessories</span>
+                    <SubMenu isMobile={false} menu="accessories"/>
+                </li>
+            </ul>
+        </>
+    ;
+
+    return (
+        (props.isMobile) ? mobileView : browserView
+    )
 
 }
 
--- components/navbar/SaltIcon.js
+++ components/navbar/SaltIcon.js
@@ -8,28 +8,26 @@ import Image from "next/image";
  */
 
 function SaltIcon(props) {
-  const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
+    const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
+    const mobileView = <div>
+        <Link href="/">
+            <a>
+                <Image src={WEBASSETS + "/assets/images/salticon.png"} alt="salt icon" width="24" height="24"/>
+            </a>
+        </Link>
+    </div>;
 
+    const browserView = <div>
+        <Link href="/">
+            <a>
+                <Image src={WEBASSETS + "/assets/images/salticon.png"} alt="salt icon" width="24" height="24"/>
+            </a>
+        </Link>
+    </div>;
 
-  const mobileView = <div>
-    <Link href="/">
-      <a>
-        <Image src={WEBASSETS + "/assets/images/salticon.png"} alt="salt icon"/>
-      </a>
-    </Link>
-  </div>;
-
-  const browserView = <div>
-    <Link href="/">
-      <a>
-        <Image src={WEBASSETS + "/assets/images/salticon.png"} alt="salt icon"/>
-      </a>
-    </Link>
-  </div>;
-
-  return (
-    (props.isMobile) ? mobileView : browserView
-  )
+    return (
+        (props.isMobile) ? mobileView : browserView
+    )
 
 }
 
