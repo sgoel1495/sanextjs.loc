c252281235087c6c96fbb1429c9c9fb313882e0e
Sambhav399 <sambhavsharma1999@gmail.com>
2022-01-08 21:22:42 +0530

    Actual Data in Swiper

--- components/swipers/MediaBuzzSwiper.js
+++ components/swipers/MediaBuzzSwiper.js
@@ -13,6 +13,39 @@ import SwiperCore, {Pagination, Navigation} from 'swiper';
 
 SwiperCore.use([Pagination, Navigation]);
 
+const actualData = [
+    {
+        link: "https://economictimes.indiatimes.com/small-biz/startups/newsbuzz/fashion-tech-firm-salt-attire-launches-its-offline-experience-store-in-gurugram/articleshow/70765514.cms",
+        title: "SALT offers tailored clothing to customer measurements",
+        image: "https://saltattire.com/assets/images/media/the-economic-times/logo/economic_times.png",
+    },
+    {
+        link: "http://www.newindianexpress.com/cities/delhi/2019/aug/21/salt-attire-one-stop-shop-for-all-your-clothing-needs-in-gurugram-2022068.html",
+        title: "One-stop shop for all your clothing needs",
+        image: "https://saltattire.com/assets/images/media/the-new-india-express/logo/the_new_india_express.png",
+    },
+    {
+        link: "https://lbb.in/delhi/awesome-homegrown-brands-on-radar",
+        title: "Bored of your usual corporate looks?",
+        image: "https://saltattire.com/assets/images/media/lbb/logo/lbb.png",
+    },
+    {
+        link: "https://yourstory.com/2017/09/dipti-tolanis-journey",
+        title: "Salt, that ingredient so essential for balance",
+        image: "https://saltattire.com/assets/images/media/yourstory/logo/yourstory.png",
+    },
+    {
+        link: "https://www.news18.com/news/lifestyle/turn-your-monotonous-office-wear-instantly-into-party-ready-look-2183283.html",
+        title: "...quick and easy way to take your work look from formal to exceptional.",
+        image: "https://saltattire.com/assets/images/media/news-18/logo/news_18.png",
+    },
+    {
+        link: "https://www.business-standard.com/article/news-ians/turn-monotonous-office-wear-into-party-ready-look-119061200529_1.html",
+        title: "Turn monotonous office wear into party ready look",
+        image: "https://saltattire.com/assets/images/media/business-standard/logo/business_standard.png",
+    },
+]
+
 function MediaBuzzSwiper(props) {
     const mobileView = null;
 
@@ -33,12 +66,18 @@ function MediaBuzzSwiper(props) {
                 navigation={true}
                 className={"mb-10"}
             >
-                {[...Array(20)].map((_, index) => {
+                {actualData.map((item, index) => {
                     return (
                         <SwiperSlide key={index} className={"mb-10"}>
-                            <a href="#" className={"flex flex-col gap-5 items-center p-10 grayscale hover:grayscale-0"}>
-                                <h5 className={'text-h5'}>Lorem Ipsum {index +1}</h5>
-                                <Image src={"https://source.unsplash.com/random"} width={"100"} height={"100"} />
+                            <a href={item.link} className={"flex flex-col items-center p-10"}>
+                                <h5 className={'text-h5 min-h-[65px] text-center'}>{item.title}</h5>
+                                <span className="relative h-[140px] w-full grayscale hover:grayscale-0">
+                                    <Image
+                                        src={item.image}
+                                        layout="fill"
+                                        objectFit="contain"
+                                    />
+                                </span>
                             </a>
                         </SwiperSlide>
                     )
--- components/swipers/ShopByLooksSwiper.js
+++ components/swipers/ShopByLooksSwiper.js
@@ -13,6 +13,59 @@ import Image from "next/image";
 
 SwiperCore.use([Pagination, Navigation, Autoplay]);
 
+const actualData = [
+    {
+        url: "#",
+        title: "A Classic",
+        image: "https://saltattire.com/assets/look-796/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Jacquard",
+        image: "https://saltattire.com/assets/look-795/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Raspberry Crush",
+        image: "https://saltattire.com/assets/look-794/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Paris Winter",
+        image: "https://saltattire.com/assets/look-793/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Ace of Spades",
+        image: "https://saltattire.com/assets/look-792/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Check Mate",
+        image: "https://saltattire.com/assets/look-791/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Classic Checks",
+        image: "https://saltattire.com/assets/look-790/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Poncho Must Have",
+        image: "https://saltattire.com/assets/look-789/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "Work & Travel Friendly",
+        image: "https://saltattire.com/assets/look-788/Full.v1.jpg"
+    },
+    {
+        url: "#",
+        title: "1 Dress Many Ways",
+        image: "https://saltattire.com/assets/look-787/Full.v1.jpg"
+    },
+]
+
 function ShopByLooksSwiper(props) {
     const mobileView = null;
 
@@ -34,20 +87,20 @@ function ShopByLooksSwiper(props) {
                 navigation={true}
                 className={"mb-10"}
             >
-                {[...Array(20)].map((_, index) => {
+                {actualData.map((item, index) => {
                     return (
                         <SwiperSlide key={index}>
-                            <a href="#" className={"flex flex-col gap-5 items-center"}>
+                            <a href={item.url} className={"flex flex-col gap-5 items-center"}>
                                 <span className={"relative h-[806px] w-full"}>
                                     <Image
-                                        src={"https://source.unsplash.com/random"}
+                                        src={item.image}
                                         width={1080}
                                         height={1080}
                                         layout="fill"
                                         objectFit="cover"
                                     />
                                 </span>
-                                <h5 className={'text-h5 italic font-500'}>Lorem Ipsum {index +1}</h5>
+                                <h5 className={'text-h5 italic font-500'}>{item.title}</h5>
                             </a>
                         </SwiperSlide>
                     )
