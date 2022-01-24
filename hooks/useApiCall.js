647af44bc6628755b5847f8e193eb0c73acb7e20
Leo Getz <jomapps.jb@gmail.com>
2022-01-25 07:10:31 +0800

    Duplicate api calls eliminated

--- hooks/useApiCall.js
+++ hooks/useApiCall.js
@@ -1,23 +1,35 @@
 /**
  * @params word is the api dictionary word, apiToken is required to make calls, queryObject has special params as may be required
  */
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {apiDictionary} from "../helpers/apiDictionary";
+import AppWideContext from "../store/AppWideContext";
 
 function useApiCall(word,apiToken=null, queryObject={}){
+    const {dataStore,updateDataStore} = useContext(AppWideContext);
     const [result,setResult] = useState(null);
+    let key = null;
+    if(queryObject)
+        key = word + apiToken + JSON.stringify(queryObject);
+    else
+        key = word + apiToken;
 
     useEffect(()=>{
         if(apiToken!=null) {
             const callObject = apiDictionary(word,apiToken,queryObject);
             if(callObject!=null) {
-                fetch(callObject.url, callObject.fetcher)
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(data => {
-                        setResult(data);
-                    });
+                if(dataStore.hasOwnProperty(key)) {
+                    setResult(JSON.parse(dataStore[key]));
+                } else {
+                    fetch(callObject.url, callObject.fetcher)
+                        .then(response => {
+                            return response.json();
+                        })
+                        .then(data => {
+                            updateDataStore(key, JSON.stringify(data));
+                            setResult(data);
+                        });
+                }
             }
         }
     },[])
