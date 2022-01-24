37b4c2f62de71b9e04a394dc5fe594e0f25f29b9
Leo Getz <jomapps.jb@gmail.com>
2022-01-25 07:19:32 +0800

    Duplicate api now using local storage as cache

--- hooks/useApiCall.js
+++ hooks/useApiCall.js
@@ -1,32 +1,33 @@
 /**
  * @params word is the api dictionary word, apiToken is required to make calls, queryObject has special params as may be required
  */
-import {useContext, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {apiDictionary} from "../helpers/apiDictionary";
-import AppWideContext from "../store/AppWideContext";
 
 function useApiCall(word,apiToken=null, queryObject={}){
-    const {dataStore,updateDataStore} = useContext(AppWideContext);
     const [result,setResult] = useState(null);
     let key = null;
-    if(queryObject)
-        key = word + apiToken + JSON.stringify(queryObject);
-    else
-        key = word + apiToken;
+    if(apiToken!=null) {
+        if (queryObject)
+            key = word + apiToken + JSON.stringify(queryObject);
+        else
+            key = word + apiToken;
+    }
 
     useEffect(()=>{
         if(apiToken!=null) {
             const callObject = apiDictionary(word,apiToken,queryObject);
             if(callObject!=null) {
-                if(dataStore.hasOwnProperty(key)) {
-                    setResult(JSON.parse(dataStore[key]));
+                const gotLocalData = localStorage.getItem(key);
+                if(gotLocalData) {
+                    setResult(JSON.parse(gotLocalData));
                 } else {
                     fetch(callObject.url, callObject.fetcher)
                         .then(response => {
                             return response.json();
                         })
                         .then(data => {
-                            updateDataStore(key, JSON.stringify(data));
+                            localStorage.setItem(key, JSON.stringify(data));
                             setResult(data);
                         });
                 }
