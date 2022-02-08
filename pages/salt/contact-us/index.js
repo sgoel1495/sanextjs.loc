94a0573abdd0f5f3f92fde64e331fb26cfc604d2
Leo Getz <jomapps.jb@gmail.com>
2022-02-08 20:59:08 +0800

    before testing

--- pages/salt/contact-us/index.js
+++ pages/salt/contact-us/index.js
@@ -1,20 +1,67 @@
-import {Fragment, useContext} from "react";
+import {Fragment, useContext, useEffect, useState} from "react";
 import PageHead from "../../../components/PageHead";
 import InfoBand from "../../../components/info-band/InfoBand";
 import LooksNavbar from "../../../components/navbar/LookNavbar";
 import CategoryHeaderImage from "../../../components/common/CategoryHeaderImage";
 import Footer from "../../../components/footer/Footer";
 import AppWideContext from "../../../store/AppWideContext";
+import { GoogleMap, Marker } from "react-google-maps";
 
 /**
  * @todo Sambhav css pls
  */
 
 function ContactUsPage(){
+    const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
     const {dataStore} = useContext(AppWideContext);
+    const category = "Contact Us";
+
+    // NavBar Controls
+    const [navControl, setNavControl] = useState(false);
+    const controller = () => setNavControl(window.scrollY > 0);
+    useEffect(() => {
+        window.addEventListener("scroll", controller);
+        return () => window.removeEventListener('scroll', controller)
+    }, []);
+
 
     const mobileView = null;
-    const browserView = null;
+    const browserView = <div>
+        <div>
+            <div>We Would Love To Hear From You!</div>
+            <div>
+                <div>
+                    Corporate Office
+                    Mon to Fri - 9:00 am to 7:00 pm
+                    Sat - 9:00 am to 2:00 pm
+                    Plot 508, Udyog Vihar Phase V,
+                    Gurugram, Haryana 122016
+                </div>
+                <div>
+                    SALT Experience Store
+                    11:00 am to 8:30 pm
+                    DLF Mega Mall (Shop No LG-51)
+                    Golf Course Road, Gurugram, Haryana 122002
+                    Open all 7 days
+                </div>
+            </div>
+            <div>
+                care@saltattire.com
+                18002709515
+            </div>
+            <div>
+                Contact Us
+            </div>
+        </div>
+        <div>
+            <GoogleMap
+                defaultZoom={12}
+                defaultCenter={{ lat: 28.501388, lng: 77.086267 }}
+            >
+                <Marker position={{ lat: 28.501388, lng: 77.086267 }} title='Salt Store' icon={WEBASSETS + "/assets/images/salt_black.png"} />
+            </GoogleMap>
+        </div>
+    </div>;
 
     return (
         <Fragment>
