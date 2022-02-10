3f6129c973597f1434230587d7d1e75eda7df624
Leo Getz <jomapps.jb@gmail.com>
2022-02-10 21:16:13 +0800

    Get Virtual appointment done. missing css

--- components/common/CategoryHeaderImage.js
+++ components/common/CategoryHeaderImage.js
@@ -31,6 +31,9 @@ function CategoryHeaderImage(props) {
         case "Privacy Policy":
             imageSource = imageSource + "images/TnC.1.jpg";
             break;
+        case "Get Virtual Appointment":
+            imageSource = imageSource + "images/ContactUs.2_v1.jpg";
+            break;
         default:
             break;
     }
@@ -41,13 +44,16 @@ function CategoryHeaderImage(props) {
             <span className={`block relative w-full h-[70vh]`}>
                 <Image src={imageSource} alt={category} layout={`fill`} objectFit={`cover`} />
             </span>
-            <div className={`absolute inset-0 flex items-center justify-start`}>
-                <div className={`bg-black pt-12 pb-6 pl-28 w-1/3 text-white font-cursive italic leading-none`}>
-                    <span className={`text-6xl`}>
-                        {category}
-                    </span>
+            {(category=="Get Virtual Appointment")
+                ?null
+                :<div className={`absolute inset-0 flex items-center justify-start`}>
+                    <div className={`bg-black pt-12 pb-6 pl-28 w-1/3 text-white font-cursive italic leading-none`}>
+                        <span className={`text-6xl`}>
+                            category
+                        </span>
+                    </div>
                 </div>
-            </div>
+            }
         </section>
     )
 }
--- pages/get-virtual-appointment/index.js
+++ pages/get-virtual-appointment/index.js
@@ -1,8 +1,95 @@
-function GetVirtualAppointmentPage(){
-    const mobileView = null;
-    const browserView = null;
+import PageHead from "../../components/PageHead";
+import InfoBand from "../../components/info-band/InfoBand";
+import LooksNavbar from "../../components/navbar/LookNavbar";
+import CategoryHeaderImage from "../../components/common/CategoryHeaderImage";
+import Footer from "../../components/footer/Footer";
+import AppWideContext from "../../store/AppWideContext";
+import {Fragment, useContext, useEffect, useState} from "react";
 
-    return props.isMobile ? mobileView : browserView
+/**
+ * @TODO FORM SUBMISSION LOGIC
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+function GetVirtualAppointmentPage(){
+    const {dataStore} = useContext(AppWideContext);
+    // NavBar Controls
+    const [navControl, setNavControl] = useState(false);
+    const controller = () => setNavControl(window.scrollY > 0);
+    useEffect(() => {
+        window.addEventListener("scroll", controller);
+        return () => window.removeEventListener('scroll', controller)
+    }, []);
+
+    const mobileView = null;
+    const browserView = <div>
+        <div>Book An Online Styling Session</div>
+        <form>
+            <div>
+                <div>
+                    <label htmlFor="date">Choose Date</label>
+                    <input id="date" type="datetime-local" />
+                    <label htmlFor="time">Preferred Time</label>
+                    <input id="time" type="time" />
+                </div>
+                <div>
+                    <label htmlFor="lastname">Last Name</label>
+                    <input id="lastname" type="text" />
+                </div>
+                <div>
+                    <label htmlFor="phonenumber">Phone Number</label>
+                    <input id="phonenumber" type="tel" />
+                </div>
+                <div>
+                    <label htmlFor="message">Anything you would like to tell us?</label>
+                    <textarea id="message" />
+                </div>
+            </div>
+            <div>
+                <div>
+                    <label htmlFor="firstname">First Name</label>
+                    <input id="firstname" type="text" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" />
+                </div>
+                <div>
+                    <label htmlFor="shoppedbefore">Have you shopped with us before?</label>
+                    <select id="shoppedbefore">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="somethingspecific">Anything you would like to tell us?</label>
+                    <textarea id="somethingspecific"/>
+                </div>
+            </div>
+            <div>
+                BOOK A VIRTUAL STYLING SESSION
+            </div>
+        </form>
+    </div>;
+
+    const category = "Get Virtual Appointment"
+
+    return (
+        <Fragment>
+            <PageHead url="/salt/get-virtual-appointment" id="virtualappointment" isMobile={dataStore.mobile}/>
+            <div className={"navigator fixed top-0 right-0 left-0 z-10 duration-300 hover:bg-white transition-colors" + [navControl ? ' bg-white/90' : ' bg-white/80']}>
+                <InfoBand/>
+                <LooksNavbar isMobile={dataStore.mobile}/>
+            </div>
+            <CategoryHeaderImage category={category}/>
+            <section className="container my-20 grid grid-cols-2 gap-x-10 gap-y-5">
+                {(dataStore.mobile) ? mobileView : browserView}
+            </section>
+            <Footer isMobile={dataStore.mobile}/>
+        </Fragment>
+    )
+
 
 }
 
