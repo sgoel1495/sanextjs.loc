c6c72df553206c55209c06d77eb916be109d6022
Shubham Goel <shubham.goel1495@gmail.com>
2022-03-23 15:12:09 +0530

    deployment

---
+++ components/new-Arrivals/ExploreNewArrivals.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExploreNewArrivals = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ExploreNewArrivals;
--- components/new-Arrivals/Index.js
+++ components/new-Arrivals/Index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NewArrivalsSwiper from "../swipers/NewArrivalsSwiper";
+import ExploreNewArrivals from "./ExploreNewArrivals";
 
 const Index = (props) => {
     return (
         <div className={"py-2"}>
             <span>NEW ARRIVALS</span>
             <NewArrivalsSwiper isMobile={true}/>
+            <ExploreNewArrivals/>
         </div>
     );
 };
