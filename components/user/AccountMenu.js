942efbd9a563a40d8f7602b24f7498e4f1e8190a
Leo Getz <jomapps.jb@gmail.com>
2022-04-10 23:43:09 +0800

    Account done

--- components/user/AccountMenu.js
+++ components/user/AccountMenu.js
@@ -1,31 +1,51 @@
 import Link from "next/link";
-import Image from "next/image";
-import Cart from "../cart/Cart";
 import React from "react";
+import AppWideContext from "../../store/AppWideContext";
 
 /**
+ * @Sambhav css please
  * @params {isMobile} props
  * @constructor
  */
 
 function AccountMenu(props){
-  const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
+    const {dataStore} = useContext(AppWideContext);
 
-  const mobileView = <div>
-    <Link href="/homepage/cart">
-      <a className={"leading-none"}>
-        <Image src={WEBASSETS + "/assets/images/user_icon.svg"} alt="usericon" width="24" height="24"/>
-        <sup>0</sup>
-      </a>
-    </Link>
-  </div>;
+    const mobileView = null;
 
   const browserView = (
-      <div>
-        <Cart isMobile={false}/>
-        <Image src={WEBASSETS + "/assets/images/usericon.png"} alt="usericon" width="24" height="24"/>
-        <sup>0</sup>
-      </div>
+      <div id="userlogindiv" className={`bg-theme-900/50 fixed inset-0 z-20`}>
+          <button className={`w-8 h-8 absolute right-10 top-10`} onClick={closeModal}>
+              <svg xmlns="http://www.w3.org/2000/svg" className={`w-8 h-8`} viewBox="0 0 24 24">
+                  <path
+                      d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"/>
+              </svg>
+          </button>
+      <Link href="/users/profile">
+          <a className={"leading-none"}> My Profile</a>
+      </Link>
+      <Link href="/users/orderhistory">
+          <a className={"leading-none"}> My Orders</a>
+      </Link>
+      <Link href="/users/wallet">
+          <a className={"leading-none"}> My Wallet</a>
+      </Link>
+      <Link href="/users/wallet">
+          <a className={"leading-none"}> Redeem Voucher</a>
+      </Link>
+      <Link href="/users/favourites">
+          <a className={"leading-none"}> My Favourites</a>
+      </Link>
+      <Link href="/users/measurements">
+          <a className={"leading-none"}> My Measurements</a>
+      </Link>
+      <Link href="/users/my-referral">
+          <a className={"leading-none"}> My Referral</a>
+      </Link>
+      <Link href="/users/logout">
+          <a className={"leading-none"}> Logout</a>
+      </Link>
+    </div>
   );
 
   return props.isMobile ? mobileView : browserView
