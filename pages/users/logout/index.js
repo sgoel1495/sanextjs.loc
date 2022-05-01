297f63ea99cee17053ea1506375d593cdd9d812f
Leo Getz <jomapps.jb@gmail.com>
2022-05-01 21:27:18 +0800

    Account login working properly

--- pages/users/logout/index.js
+++ pages/users/logout/index.js
@@ -1,13 +1,19 @@
-import React, {useContext} from "react";
 import AppWideContext from "../../../store/AppWideContext";
+import {useRouter} from "next/router";
+import {Fragment, useContext, useEffect} from "react";
 
-function LogoutPage(){
-    const {dataStore} = useContext(AppWideContext);
-
-    const mobileView=null;
-    const browserView=null;
-
-    return (dataStore.mobile) ? mobileView : browserView;
+function Logout() {
+    const {updateDataStore} = useContext(AppWideContext);
+    const router = useRouter();
+    useEffect(() => {
+        const userData={
+            "contact": null
+        };
+        updateDataStore("userData", userData);
+        localStorage.setItem("userData", JSON.stringify(userData));
+        router.replace("/");
+    }, []);
+    return <Fragment>Logging out ...</Fragment>;
 }
 
-export default LogoutPage;
+export default Logout;
