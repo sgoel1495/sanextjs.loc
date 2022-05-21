e16b0487a90ef1f00ab4ebe0f1f8606ea2584cd6
Shubham Goel <shubham.goel1495@gmail.com>
2022-05-21 18:22:36 +0530

    build fixes

--- pages/_app.js
+++ pages/_app.js
@@ -16,14 +16,14 @@ function MyApp({Component, pageProps}) {
     dataStoreDefault.apiToken = pageProps.apiToken;
     const [dataStore, setDataStore] = useState(dataStoreDefault);
     const [refresh, setRefresh] = useState(true);
-    
+
     const updateDataStore = useCallback((key, value) => {
         dataStore[key] = value;
         setDataStore({...dataStore});
         setRefresh(!refresh);
-    },[dataStore, refresh])
+    }, [dataStore, refresh])
 
-    const botuiScripts = ()=>{
+    const botuiScripts = () => {
         return null;
         return <Fragment>
             <Script src="/static/js/bot_js/vue.min.js"/>
@@ -33,15 +33,15 @@ function MyApp({Component, pageProps}) {
         </Fragment>
     }
 
-    useEffect(()=>{
-        if(isMobile!=dataStoreDefault.mobile)
-            updateDataStore("mobile",isMobile);
-        if(dataStore.userData.contact==null){
-            const userServe=dataStore.userServe;
+    useEffect(() => {
+        if (isMobile != dataStoreDefault.mobile)
+            updateDataStore("mobile", isMobile);
+        if (dataStore.userData.contact == null) {
+            const userServe = dataStore.userServe;
             userServe.temp_user_id = Date.now().toString();
-            updateDataStore("userServe",userServe);
+            updateDataStore("userServe", userServe);
         }
-    },[dataStoreDefault.mobile]);
+    }, [dataStoreDefault.mobile]);
 
     return <Fragment>
         <AppWideContext.Provider value={{
@@ -50,82 +50,90 @@ function MyApp({Component, pageProps}) {
             <Component {...pageProps} />
         </AppWideContext.Provider>
         <Script src="https://connect.facebook.net/en_US/all.js?hash=c3e73dbaa85ad58c8934ca9e6c6f542c"
-                crossOrigin="anonymous" />
+                crossOrigin="anonymous"/>
         <Script src="https://connect.facebook.net/signals/config/159270414641031?v=2.9.60&amp;r=stable" async=""/>
         <Script async="" src="https://connect.facebook.net/en_US/fbevents.js"/>
 
         <Script id="facebook-jssdk" src="https://connect.facebook.net/en_US/all.js"/>
-        <Script type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify({
-            "@context": "http://www.schema.org",
-            "@type": "WebSite",
-            "name": "Salt Attire",
-            "url": "https://saltattire.com/"
-        }) }}/>
-        <script type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify({
-                "@context": "http://www.schema.org",
-                "@type": "Organization",
-                "name": "Salt Attire",
-                "url": "https://saltattire.com/",
-                "logo": "https://saltattire.com/assets/SALT_logo.png"
-        }) }}/>
-        <Script type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify({
-                "@context": "http://www.schema.org",
-                "@type": "LocalBusiness",
-                "name": "Salt Attire",
-                "url": "https://saltattire.com/",
-                "logo": "https://saltattire.com/assets/SALT_logo.png",
-                "image": "https://saltattire.com/assets/look-224/Full.v1.jpg",
-                "priceRange" : "INR",
-                "telephone": "18002709515",
-                "description": " Our design team has a simple philosophy, design a nine to nine wardrobe for the contemporary working woman. We have anticipated your needs, your lifestyle, your schedule to create designs which are functional, sophisticated and chic. We focus on clean cuts with finer detailing. We understand the need for functionality without compromising on the aesthetic appeal.",
-                "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "Plot 508, Udyog Vihar Phase V",
-                "addressLocality": "Gurugram",
-                "addressRegion": "Haryana",
-                "postalCode": "122022",
-                "addressCountry": "India"
-            },
-                "hasMap": "https://g.page/saltattire?share",
-                "openingHours": "Mo, Tu, We, Th, Fr,  09:00-07:00, Sa 09:00-02:00",
-                "contactPoint": {
-                "@type": "ContactPoint",
-                "contactType": "Customer Service",
-                "telephone": "18002709515"
-            },
-                "aggregateRating": {
-                "ratingValue": "4.6",
-                "reviewCount": "5"
-            }
-        }) }}/>
-        <script type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify({
-                "@context": "http://www.schema.org",
-                "@type": "LocalBusiness",
-                "name": "Salt Attire",
-                "url": "https://saltattire.com/",
-                "logo": "https://saltattire.com/assets/SALT_logo.png",
-                "image": "https://saltattire.com/assets/look-224/Full.v1.jpg",
-                "priceRange" : "INR",
-                "telephone": "18002709515",
-                "description": " Our design team has a simple philosophy, design a nine to nine wardrobe for the contemporary working woman. We have anticipated your needs, your lifestyle, your schedule to create designs which are functional, sophisticated and chic. We focus on clean cuts with finer detailing. We understand the need for functionality without compromising on the aesthetic appeal.",
-                "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "Plot 508, Udyog Vihar Phase V",
-                "addressLocality": "Gurugram",
-                "addressRegion": "Haryana",
-                "postalCode": "122022",
-                "addressCountry": "India"
-            },
-                "contactPoint": {
-                "@type": "ContactPoint",
-                "contactType": "Customer Service",
-                "telephone": "18002709515"
-            }
-        }) }}/>
+        <Script id="website" type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                        "@context": "http://www.schema.org",
+                        "@type": "WebSite",
+                        "name": "Salt Attire",
+                        "url": "https://saltattire.com/"
+                    })
+                }}/>
+        <script id="Organization" type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                        "@context": "http://www.schema.org",
+                        "@type": "Organization",
+                        "name": "Salt Attire",
+                        "url": "https://saltattire.com/",
+                        "logo": "https://saltattire.com/assets/SALT_logo.png"
+                    })
+                }}/>
+        <Script id={"LocalBusiness"} type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                        "@context": "http://www.schema.org",
+                        "@type": "LocalBusiness",
+                        "name": "Salt Attire",
+                        "url": "https://saltattire.com/",
+                        "logo": "https://saltattire.com/assets/SALT_logo.png",
+                        "image": "https://saltattire.com/assets/look-224/Full.v1.jpg",
+                        "priceRange": "INR",
+                        "telephone": "18002709515",
+                        "description": " Our design team has a simple philosophy, design a nine to nine wardrobe for the contemporary working woman. We have anticipated your needs, your lifestyle, your schedule to create designs which are functional, sophisticated and chic. We focus on clean cuts with finer detailing. We understand the need for functionality without compromising on the aesthetic appeal.",
+                        "address": {
+                            "@type": "PostalAddress",
+                            "streetAddress": "Plot 508, Udyog Vihar Phase V",
+                            "addressLocality": "Gurugram",
+                            "addressRegion": "Haryana",
+                            "postalCode": "122022",
+                            "addressCountry": "India"
+                        },
+                        "hasMap": "https://g.page/saltattire?share",
+                        "openingHours": "Mo, Tu, We, Th, Fr,  09:00-07:00, Sa 09:00-02:00",
+                        "contactPoint": {
+                            "@type": "ContactPoint",
+                            "contactType": "Customer Service",
+                            "telephone": "18002709515"
+                        },
+                        "aggregateRating": {
+                            "ratingValue": "4.6",
+                            "reviewCount": "5"
+                        }
+                    })
+                }}/>
+        <script id={"LocalBusiness2"} type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                        "@context": "http://www.schema.org",
+                        "@type": "LocalBusiness",
+                        "name": "Salt Attire",
+                        "url": "https://saltattire.com/",
+                        "logo": "https://saltattire.com/assets/SALT_logo.png",
+                        "image": "https://saltattire.com/assets/look-224/Full.v1.jpg",
+                        "priceRange": "INR",
+                        "telephone": "18002709515",
+                        "description": " Our design team has a simple philosophy, design a nine to nine wardrobe for the contemporary working woman. We have anticipated your needs, your lifestyle, your schedule to create designs which are functional, sophisticated and chic. We focus on clean cuts with finer detailing. We understand the need for functionality without compromising on the aesthetic appeal.",
+                        "address": {
+                            "@type": "PostalAddress",
+                            "streetAddress": "Plot 508, Udyog Vihar Phase V",
+                            "addressLocality": "Gurugram",
+                            "addressRegion": "Haryana",
+                            "postalCode": "122022",
+                            "addressCountry": "India"
+                        },
+                        "contactPoint": {
+                            "@type": "ContactPoint",
+                            "contactType": "Customer Service",
+                            "telephone": "18002709515"
+                        }
+                    })
+                }}/>
         {botuiScripts()}
 
     </Fragment>
