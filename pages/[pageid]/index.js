740b6a585c08eba2ab111305138a523eff9fe6c7
Leo Getz <jomapps.jb@gmail.com>
2022-07-03 17:45:43 +0800

    shoppage mobile done

--- pages/[pageid]/index.js
+++ pages/[pageid]/index.js
@@ -1,5 +1,5 @@
 import {useRouter} from "next/router";
-import React, {Fragment, useContext} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 
 import ShopPage from "../../components/shop-page/ShopPage";
 import ProductPage from "../../components/product-page/ProductPage";
@@ -9,14 +9,23 @@ function PageById(){
     const router = useRouter();
     const query = router.query;
 
-    return <Fragment>
-        {(router && query && query.pageid && query.pageid.startsWith("shop-"))
-            ?<ShopPage  hpid={query.pageid}/>
-            :(router && query && query.pageid && query.pageid.startsWith("mimoto-"))
-                ?<MimotoPage  hpid={query.pageid}/>
-                :<ProductPage hpid={query.pageid}/>
-        }
-    </Fragment>;
+    const [forceRefresh,setForceRefresh] = useState(false)
+    useEffect(()=>{
+        if(!forceRefresh)
+            setForceRefresh(true)
+    },[forceRefresh])
+
+
+    return forceRefresh
+        ?<Fragment>
+            {(router && query && query.pageid && query.pageid.startsWith("shop-"))
+                ?<ShopPage  hpid={query.pageid}/>
+                :(router && query && query.pageid && query.pageid.startsWith("mimoto-"))
+                    ?<MimotoPage  hpid={query.pageid}/>
+                    :<ProductPage hpid={query.pageid}/>
+            }
+        </Fragment>
+        : null
 }
 
 export default PageById;
