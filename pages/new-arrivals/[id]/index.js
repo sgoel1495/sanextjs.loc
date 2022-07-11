3071876525f25bdf5cc4791fd3e0d19798daee75
Shubham Goel <shubham.goel1495@gmail.com>
2022-07-11 16:33:46 +0530

    new arrivals

--- pages/new-arrivals/[id]/index.js
+++ pages/new-arrivals/[id]/index.js
@@ -9,11 +9,12 @@ import ProductCard from "../../../components/new-Arrivals/ProductCard";
 import Footer from "../../../components/footer/Footer";
 import {apiCall} from "../../../helpers/apiCall";
 import {useRouter} from "next/router";
+import MobileProductCard from "../../../components/shop-page/ProductCard"
 
-function NewArrivalsIdPage(props){
+function NewArrivalsIdPage(props) {
     const category = "new-arrivals"
     const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
-    const { dataStore } = useContext(AppWideContext);
+    const {dataStore} = useContext(AppWideContext);
     const [data, setData] = useState([]);
     const [carousal, setCarousal] = useState(props.carousal);
     const router = useRouter()
@@ -37,18 +38,18 @@ function NewArrivalsIdPage(props){
             ""look_mo_thumb"": ""/assets/Outerwear-Gallant-Suit-Set-FullSuitSet/mo.new.jpg""
         }
 */
-    console.log("DATA",data)
-    useEffect(()=>{
+    console.log("DATA", data)
+    useEffect(() => {
         const fetchData = async () => {
             let gotData = false;
             const callObject = await apiCall("getProducts", process.env.API_TOKEN, {category: "new-arrivals", limit: 10000, skip: 0})
             if (callObject.hasOwnProperty("response") && callObject.response.hasOwnProperty("data"))
                 gotData = true;
 
-            if(gotData)
+            if (gotData)
                 setCarousal(callObject.new_arr_carousal)
         }
-        if(router.query.id) {
+        if (router.query.id) {
             fetchData()
                 .then(resp => {
                     console.log("Carousal loaded")
@@ -62,7 +63,7 @@ function NewArrivalsIdPage(props){
                 })
                 .catch(e => console.log(e.msg))
         }
-    },[dataStore.apiToken,router.query.id])
+    }, [dataStore.apiToken, router.query.id])
 
     const loader = <span className={"col-span-3 flex justify-center items-center"} key="loader">
                             <span className={"block relative w-14 aspect-square"}>
@@ -71,12 +72,34 @@ function NewArrivalsIdPage(props){
                             </span>
                     </span>
 
-    const mobileView = null;
+    const mobileView = <section className={"bg-[#faf4f0] pb-20"}>
+        <div className="flex tracking-widest text-h4 items-center justify-center">
+            <span>~</span>
+            <span className="text-center p-2">Newly Launched<br/>Products</span>
+            <span>~</span>
+        </div>
+        {(data)
+            ? <main className={`px-5`}>
+                {data.filter(prod => prod.is_visible).slice(0, 8).map((prod, index) => {
+                    return <div className={"py-4"}>
+                        <MobileProductCard prod={prod} key={index} isMobile={true} wide={index <= 7}/>
+                    </div>
+                })}
+                <div className={"grid grid-cols-2 gap-5"}>
+                    {data.filter(prod => prod.is_visible).slice(8).map((prod, index) => {
+                        return <div className={"py-4"}>
+                            <MobileProductCard prod={prod} key={index} isMobile={true}/>
+                        </div>
+                    })}
+                </div>
+
+            </main>
+            : loader
+        }
+    </section>;
     const browserView = (
         <>
-            <PageHead url="//new-arrivals/all" id="new-arrivals-all" isMobile={dataStore.mobile} />
-            <Header type={dataStore.mobile ? "minimal" : ""} isMobile={dataStore.mobile} />
-            <HomePageHeaderSwiper page={"newArrival"} isMobile={dataStore.mobile} slides={carousal} />
+
             <section className={`bg-[#E6E1DB] pb-20`}>
                 <BlockHeader
                     line
@@ -86,19 +109,24 @@ function NewArrivalsIdPage(props){
                     <span className={"tracking-widest text-h4 uppercase"}>New Arrivals</span>
                 </BlockHeader>
                 {(data)
-                    ?<main className={`px-10 grid grid-cols-3 gap-10`}>
+                    ? <main className={`px-10 grid grid-cols-3 gap-10`}>
                         {data && data.map((prod, index) => {
                             return <ProductCard prod={prod} isMobile={dataStore.mobile} key={index} isAccessory={false}/>
                         })}
                     </main>
-                    :loader
+                    : loader
                 }
             </section>
-            <Footer isMobile={dataStore.mobile} />
         </>
     );
 
-    return dataStore.mobile ? mobileView : browserView
+    return <>
+        <PageHead url="//new-arrivals/all" id="new-arrivals-all" isMobile={dataStore.mobile}/>
+        <Header type={dataStore.mobile ? "minimal" : ""} isMobile={dataStore.mobile}/>
+        <HomePageHeaderSwiper page={"newArrival"} isMobile={dataStore.mobile} slides={carousal}/>
+        {dataStore.mobile ? mobileView : browserView}
+        <Footer isMobile={dataStore.mobile}/>
+    </>
 }
 
 export default NewArrivalsIdPage
