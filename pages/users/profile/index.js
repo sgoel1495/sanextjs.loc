26834d5ce16bfdf4ce80a1e8c0fe521d21782589
Rahul Bhatt <rahul.bhatt@careerdose.com>
2022-07-13 17:05:07 +0530

    users/profile done

--- pages/users/profile/index.js
+++ pages/users/profile/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, useEffect, useState } from "react";
+import React, {Fragment, useContext, useEffect, useState} from "react";
 import AppWideContext from "../../../store/AppWideContext";
 import PageHead from "../../../components/PageHead";
 import Header from "../../../components/navbar/Header";
@@ -6,23 +6,35 @@ import Footer from "../../../components/footer/Footer";
 import UsersSideMenu from "../../../components/user/UsersSideMenu";
 import ContactInformation from "../../../components/user/ContactInformation";
 import DefaultAddressBookInformation from "../../../components/user/DefaultAddressBookInformation";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import UserPageTemplate from "../../../components/user/UserPageTemplate";
+import UserScreenTemplate from "../../../components/user/mobile/UserScreenTemplate";
+import formatTwoDecimal from "../../../helpers/formatTwoDecimal";
 
 function UsersProfilePage() {
     const router = useRouter();
-    const { dataStore } = useContext(AppWideContext);
+    const {dataStore} = useContext(AppWideContext);
     useEffect(() => {
         if (dataStore.userData.contact == null)
             router.replace("/"); //illegal direct access
     }, [dataStore.userData.contact, router])
 
-    const mobileView = null;
+    const mobileView = () => {
+        return (<UserScreenTemplate>
+            <ContactInformation mobile={true}/>
+            {/*<MyWalletInformation/>*/}
+            <div className={"p-4 bg-[#f1f2f3]"}>
+                <p className="text-xl font-500 mb-2 mt-1 w-full">My Wallet</p>
+                <p className="font-700 pb-10">{dataStore.currSymbol} {formatTwoDecimal(dataStore.userWallet.WalletAmount)}</p>
+            </div>
+            <DefaultAddressBookInformation mobile={true} manage={true}/>
+        </UserScreenTemplate>)
+    }
     const browserView = () => {
         return (
             <UserPageTemplate>
-                <ContactInformation mobile={false} />
-                <DefaultAddressBookInformation mobile={false} manage={true} />
+                <ContactInformation mobile={false}/>
+                <DefaultAddressBookInformation mobile={false} manage={true}/>
             </UserPageTemplate>
         );
     }
@@ -30,9 +42,9 @@ function UsersProfilePage() {
 
     return (
         <Fragment>
-            <PageHead url={"/users/profile"} id={"profile"} isMobile={dataStore.mobile} />
-            <Header type={"shopMenu"} />
-            {(dataStore.mobile) ? mobileView : browserView()}
+            <PageHead url={"/users/profile"} id={"profile"} isMobile={dataStore.mobile}/>
+            <Header type={dataStore.mobile?"minimal":"shopMenu"} isMobile={dataStore.mobile}/>
+            {(dataStore.mobile) ? mobileView() : browserView()}
             <Footer isMobile={dataStore.mobile}/>
         </Fragment>
     )
