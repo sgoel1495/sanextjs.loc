d3ccca02fbdc049182748fb8145f08c26ee83bdf
Shubham Goel <shubham.goel1495@gmail.com>
2022-07-14 14:00:33 +0530

    looks page mobile

---
+++ components/looks-page/ExpandedLook.js
@@ -0,0 +1,133 @@
+import React, {useContext, useEffect, useRef} from 'react';
+import Link from "next/link";
+import BlockHeader from "../common/blockHeader";
+import appSettings from "../../store/appSettings";
+import AppWideContext from "../../store/AppWideContext";
+import Image from "next/image";
+
+const LookDataBlockImage = (props) => (
+    <span className={`block relative w-full h-full aspect-square`}>
+        <Image src={props.src} alt={props.name} layout={`fill`} objectFit={`cover`}/>
+    </span>
+)
+
+const ExpandedLook = ({expandLook, setExpandLook, data, isMobile}) => {
+    const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
+    const {dataStore} = useContext(AppWideContext);
+    const expandedRef = useRef(null);
+    const currCurrency = dataStore.currCurrency;
+    const currencyData = appSettings("currency_data");
+    const currencySymbol = currencyData[currCurrency].curr_symbol;
+
+    useEffect(() => {
+        if (expandedRef.current) {
+            const element =
+                expandedRef.current.getBoundingClientRect().top + window.scrollY - (document.getElementsByClassName("navigator")[0] ? document.getElementsByClassName("navigator")[0].getBoundingClientRect().bottom : 0)
+            window.scroll({
+                top: element,
+                behavior: "smooth"
+            })
+        }
+    }, [expandedRef])
+
+    const leadTextStyle = "text-h5 font-600";
+    const textStyle = "text-xs";
+    const buyNowButtonStyle = "bg-black/5 font-600 text-xs tracking-widest px-5 py-3 block mt-6";
+
+    let products = null;
+    let prod = null;
+    let prodDetails = null;
+    // the product
+    if (expandLook.products && expandLook.products.length > 0) {
+        prod = expandLook.products[0];
+        prodDetails = data.prod[prod];
+        products = (
+            <>
+                <div className={`grid grid-cols-2 place-items-center`}>
+                    <LookDataBlockImage src={WEBASSETS + "/assets/" + prod + "/square-crop.jpg"}
+                                        alt={prodDetails.name}/>
+                    <div className={`text-center`}>
+                        <p className={leadTextStyle}>{prodDetails.name}</p>
+                        <p className={textStyle}>{prodDetails.tag_line}</p>
+                        <p className={textStyle}>{currencySymbol}{(currCurrency === "inr") ? prodDetails.price : prodDetails.usd_price}</p>
+                        <Link href={"/" + prod}>
+                            <a className={`${buyNowButtonStyle}`}>BUY NOW</a>
+                        </Link>
+                    </div>
+                </div>
+            </>
+        );
+    }
+    // when the product has another item with it like top and bottom
+    if (expandLook.products && expandLook.products.length > 1) {
+        prod = expandLook.products[1];
+        prodDetails = data.prod[prod];
+        products = (
+            <>
+                {products}
+                <div className={`grid grid-cols-2 place-items-center`}>
+                    <div className={`text-center`}>
+                        <p className={leadTextStyle}>{prodDetails.name}</p>
+                        <p className={textStyle}>{prodDetails.tag_line}</p>
+                        <p className={textStyle}>{currencySymbol}{(currCurrency === "inr") ? prodDetails.price : prodDetails.usd_price}</p>
+                        <Link href={"/" + prod}>
+                            <a className={`${buyNowButtonStyle}`}>BUY NOW</a>
+                        </Link>
+                    </div>
+                    <LookDataBlockImage src={WEBASSETS + "/assets/" + prod + "/square-crop.jpg"}
+                                        alt={prodDetails.name}/>
+                </div>
+            </>
+        );
+    }
+
+    const mobileView = <>
+        <span className={"block relative w-full aspect-square"}>
+            <Image src={WEBASSETS + expandLook.bg_img_path} layout={`fill`} objectFit={`cover`}/>
+            <span className={"absolute top-2 right-3 font-900 text-white text-lg"} onClick={() => setExpandLook({})}>
+                X
+            </span>
+        </span>
+        <div className={"relative bg-white px-5 py-6 pb-12 text-center"}>
+            <span className={"block text-[#b77b3c] text-[20.5px] font-cursive italic mb-6"}>
+                ~Items Featured~
+            </span>
+            <span className={"absolute top-2 right-3 text-xl font-800"} onClick={() => setExpandLook({})}>X</span>
+            {products}
+        </div>
+    </>
+
+    const browserView = <>
+        <button
+            onClick={() => setExpandLook({})}
+            className={`absolute top-0 right-0 uppercase float-right text-black/50 flex gap-x-2`}
+        >
+            Close
+            <svg xmlns="http://www.w3.org/2000/svg" className={`w-6 h-6`} fill={"currentColor"}
+                 viewBox="0 0 24 24">
+                <path
+                    d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"/>
+            </svg>
+        </button>
+        <BlockHeader
+            space={"py-10"}
+            titleStyle={"font-600 flex justify-center items-center gap-3 leading-none"}
+        >
+            <span className={"text-h1"}>~</span>
+            <span className={'font-cursive italic text-h1'}>{expandLook.heading}</span>
+            <span className={"text-h1"}>~</span>
+        </BlockHeader>
+        <div className={`grid grid-cols-2`}>
+            <LookDataBlockImage src={WEBASSETS + expandLook.img_path} alt={expandLook.name}/>
+            <div className={`place-self-center bg-white h-fit w-2/3`}>
+                {products}
+            </div>
+        </div>
+    </>
+    return <div className={isMobile ? "col-span-2" : `col-span-3 relative`} ref={expandedRef}>
+        {isMobile ? mobileView : browserView}
+    </div>
+
+};
+
+export default ExpandedLook;
--- pages/looks/index.js
+++ pages/looks/index.js
@@ -1,216 +1,60 @@
-import React, { Fragment, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import PageHead from "../../components/PageHead";
-import AppWideContext from "../../store/AppWideContext";
 import Footer from "../../components/footer/Footer";
-import appSettings from "../../store/appSettings";
 import Image from "next/image";
-import Link from "next/link";
 import BlockHeader from "../../components/common/blockHeader";
-import WishListButton from "../../components/common/WishListButton";
 import Header from "../../components/navbar/Header";
-import { apiCall } from "../../helpers/apiCall";
+import {apiCall} from "../../helpers/apiCall";
 
-import sleep from "../../helpers/sleep";
-import Loader from "../../components/common/Loader";
+import {isMobile} from "react-device-detect";
+import LooksItem from "../../components/looks-page/LooksItem";
 
-const LookDataBlockImage = (props) => (
-    <span className={`block relative w-full h-full aspect-square`}>
-        <Image src={props.src} alt={props.name} layout={`fill`} objectFit={`cover`} />
-    </span>
-)
+
 
 function LooksPage(props) {
     const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
-    const { dataStore } = useContext(AppWideContext);
     const [data, setData] = useState(props.data);
-    const [expandLook, setExpandLook] = useState(null);
-    const expandedRef = useRef(null);
+    const [mobile, setMobile] = useState(false)
 
-    const currCurrency = dataStore.currCurrency;
-    const currencyData = appSettings("currency_data");
-    const currencySymbol = currencyData[currCurrency].curr_symbol;
-    /*
-        useEffect(()=>{
-    
-            if(
-                !data
-                && dataStore.apiToken
-                && pagination
-            )
-                fetchData()
-                    .then(resp=>{
-                        setData(resp)
-                    })
-                    .catch(e=>console.log(e.message))
-                    .finally(()=>{
-                        console.log("Full Data Loaded")
-                    })
-    
-        },[data, dataStore.apiToken, pagination])
-    */
+    useEffect(() => {
+        setMobile(isMobile)
+    }, [])
 
     const loader = <span className={"col-span-3 flex justify-center items-center"} key="loader">
         <span className={"block relative w-14 aspect-square"}>
             <Image src={WEBASSETS + "/assets/images/loader.gif"} layout={`fill`} objectFit={`cover`}
-                alt={"loader"} />
+                   alt={"loader"}/>
         </span>
     </span>
 
-    useEffect(() => {
-        if (expandedRef.current) {
-            const element =
-                expandedRef.current.getBoundingClientRect().top + window.scrollY - document.getElementsByClassName("navigator")[0].getBoundingClientRect().bottom
-            window.scroll({
-                top: element,
-                behavior: "smooth"
-            })
-        }
-    }, [expandLook])
-
-
-    const expandData = () => {
-
-        const leadTextStyle = "text-h5 font-600";
-        const textStyle = "text-xs";
-        const buyNowButtonStyle = "bg-black/5 font-600 text-xs tracking-widest px-5 py-3 block mt-6";
-
-        let products = null;
-        let prod = null;
-        let prodDetails = null;
-        // the product
-        if (expandLook.products && expandLook.products.length > 0) {
-            prod = expandLook.products[0];
-            prodDetails = data.prod[prod];
-            products = (
-                <>
-                    <div className={`grid grid-cols-2 place-items-center`}>
-                        <LookDataBlockImage src={WEBASSETS + "/assets/" + prod + "/square-crop.jpg"}
-                            alt={prodDetails.name} />
-                        <div className={`text-center`}>
-                            <p className={leadTextStyle}>{prodDetails.name}</p>
-                            <p className={textStyle}>{prodDetails.tag_line}</p>
-                            <p className={textStyle}>{currencySymbol}{(currCurrency === "inr") ? prodDetails.price : prodDetails.usd_price}</p>
-                            <Link href={"/" + prod}>
-                                <a className={`${buyNowButtonStyle}`}>BUY NOW</a>
-                            </Link>
-                        </div>
-                    </div>
-                </>
-            );
-        }
-        // when the product has another item with it like top and bottom
-        if (expandLook.products && expandLook.products.length > 1) {
-            prod = expandLook.products[1];
-            prodDetails = data.prod[prod];
-            products = (
-                <>
-                    {products}
-                    <div className={`grid grid-cols-2 place-items-center`}>
-                        <div className={`text-center`}>
-                            <p className={leadTextStyle}>{prodDetails.name}</p>
-                            <p className={textStyle}>{prodDetails.tag_line}</p>
-                            <p className={textStyle}>{currencySymbol}{(currCurrency === "inr") ? prodDetails.price : prodDetails.usd_price}</p>
-                            <Link href={"/" + prod}>
-                                <a className={`${buyNowButtonStyle}`}>BUY NOW</a>
-                            </Link>
-                        </div>
-                        <LookDataBlockImage src={WEBASSETS + "/assets/" + prod + "/square-crop.jpg"}
-                            alt={prodDetails.name} />
-                    </div>
-                </>
-            );
-        }
-        return (
-            <div className={`col-span-3 relative`} ref={expandedRef}>
-                <button
-                    onClick={() => setExpandLook(null)}
-                    className={`absolute top-0 right-0 uppercase float-right text-black/50 flex gap-x-2`}
-                >
-                    Close
-                    <svg xmlns="http://www.w3.org/2000/svg" className={`w-6 h-6`} fill={"currentColor"}
-                        viewBox="0 0 24 24">
-                        <path
-                            d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z" />
-                    </svg>
-                </button>
-                <BlockHeader
-                    space={"py-10"}
-                    titleStyle={"font-600 flex justify-center items-center gap-3 leading-none"}
-                >
-                    <span className={"text-h1"}>~</span>
-                    <span className={'font-cursive italic text-h1'}>{expandLook.heading}</span>
-                    <span className={"text-h1"}>~</span>
-                </BlockHeader>
-                <div className={`grid grid-cols-2`}>
-                    <LookDataBlockImage src={WEBASSETS + expandLook.img_path} alt={expandLook.name} />
-                    <div className={`place-self-center bg-white h-fit w-2/3`}>
-                        {products}
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    const lookData = () => {
-        let showLookData = null;
-        let look_ids = []
-        if (!data || !data.hasOwnProperty("look") || data.look.length < 1) return null;
-        else {
-            data.look.forEach((look, index) => {
-                look_ids.push(look.look_id)
-                showLookData = (
-                    <>
-                        {showLookData}
-                        <div
-                            onClick={() => setExpandLook(look)}
-                            className={`relative group cursor-pointer z-0`}
-                            id={look.look_id}
-                        >
-                            <WishListButton className={`absolute right-4 top-4 z-10`} pid={look.look_id} />
-                            <LookDataBlockImage src={WEBASSETS + look.img_path} alt={look.name} />
-                            <div
-                                className={"hidden group-hover:grid place-items-center absolute inset-0 opacity-95 text-white text-center font-600 tracking-wider"}
-                                style={{ background: look.bg_color }}>
-                                <div className={`self-end`}>
-                                    <p className={`mb-2 text-h5`}>{look.heading}</p>
-                                    <p className={`text-h5 font-cursive italic`}>{look.details}</p>
-                                </div>
-                                <p className={`uppercase text-sm`}>{'>'} Shop the look</p>
-                            </div>
-                        </div>
-                        {(index % 3 === 2 && expandLook && look_ids.slice(index - 2, index + 1).includes(expandLook.look_id)) ? expandData() : null}
-                    </>
-                );
-            });
-        }
-        return showLookData;
-    }
-
     const mobileView = <Fragment>
-        <PageHead url="/looks" id="looks" isMobile={true} />
-        <Header type={"minimal"} />
-        <section className={`bg-[#E6E1DB] py-20 overflow-auto`}>
+        <section className={`bg-[#222222] overflow-auto`}>
+            <span className={"block relative w-full aspect-square"}>
+                <Image src={WEBASSETS + "/assets/images/looks/looks.banner_v1.jpg"} layout={`fill`} objectFit={`cover`}/>
+            </span>
             <BlockHeader
-                space={"py-5"}
-                titleStyle={"text-center py-10 tracking-wider"}
+                space={"py-[1.125rem]"}
+                titleStyle={"text-center tracking-wider"}
             >
-                <h3 className={`text-h4 font-600`}>SHOP THE LOOK</h3>
-                <h4 className={`text-h6 text-[#a76b2c] uppercase leading-none font-600`}>Looks <span
+                <h3 className={`text-h4 font-400 text-white tracking-tight capitalize mb-1`}>Shop The Look</h3>
+                <h4 className={`text-h6 text-[#fcc4a3] uppercase leading-none font-600`}>Looks <span
                     className={`font-cursive italic text-h3 lowercase`}>we</span> Love</h4>
             </BlockHeader>
+            <div className={"text-center py-2.5"}>
+                <span className={"text-white bg-[#333333] text-[0.9rem] py-2.5 px-[5%]"}>
+                    ~Explore~
+                </span>
+            </div>
             {(data)
-                ? <main className={`px-10 grid grid-cols-3 gap-7`} >
-                    {lookData()}
+                ? <main className={`grid grid-cols-2`}>
+                    <LooksItem data={data} isMobile={mobile}/>
                 </main>
                 : loader
             }
         </section>
-        <Footer isMobile={true} />
     </Fragment>
 
     const browserView = <Fragment>
-        <PageHead url="/looks" id="looks" isMobile={false} />
-        <Header type={"shopMenu"} />
         <section className={`bg-[#E6E1DB] py-20 overflow-auto`}>
             <BlockHeader
                 space={"py-5"}
@@ -221,34 +65,28 @@ function LooksPage(props) {
                     className={`font-cursive italic text-h3 lowercase`}>we</span> Love</h4>
             </BlockHeader>
             {(data)
-                ? <main className={`px-10 grid grid-cols-3 gap-7`} >
-                    {lookData()}
+                ? <main className={`px-10 grid grid-cols-3 gap-7`}>
+                    <LooksItem data={data} isMobile={mobile}/>
                 </main>
                 : loader
             }
         </section>
-        <Footer isMobile={false} />
+
     </Fragment>
 
-    const [forceRefresh, setForceRefresh] = useState(false)
-    useEffect(() => {
-        if (!forceRefresh)
-            setForceRefresh(true)
-    }, [forceRefresh])
-
-
-    return forceRefresh
-        ? dataStore.mobile
-            ? mobileView
-            : browserView
-        : <Loader />
+    return <>
+        <PageHead url="/looks" id="looks" isMobile={mobile}/>
+        <Header type={mobile ? "shopMenu" : "minimal"} isMobile={mobile}/>
+        {mobile ? mobileView : browserView}
+        <Footer isMobile={true}/>
+    </>
 
 }
 
 export async function getStaticProps() {
     const fetchData = async () => {
         let gotData = false;
-        const callObject = await apiCall("getLooksData", process.env.API_TOKEN, { look_id: "", limit: 10000, skip: 0 })
+        const callObject = await apiCall("getLooksData", process.env.API_TOKEN, {look_id: "", limit: 10000, skip: 0})
         if (
             callObject.hasOwnProperty("response")
             && callObject.response.hasOwnProperty("look")
@@ -268,43 +106,3 @@ export async function getStaticProps() {
 
 
 export default LooksPage;
-
-/*
-each look has
-after_color: ""
-bg_color: "#0c172f"
-bg_img_path: "/assets/look-757/Collage.v1.jpg"
-color: "#ffffff"
-details: "Aadya"
-heading: "Green Sheen"
-img_path: "/assets/look-757/Full.v1.jpg"
-is_img_left: false
-look_id: "look-757"
-name: "Green Sheen"
-products: ['Dresses-Aadya-RawSilkFestiveFitandA-LineDress']
-template: 2
-
-after_color: ""
-bg_color: "#5b4351"
-bg_img_path: "/assets/look-768/Collage.v1.jpg"
-color: "#ffffff"
-details: "Hibiscus,Earnest-Black"
-heading: "Subtle Floral"
-img_path: "/assets/look-768/Full.v1.jpg"
-is_img_left: true
-look_id: "look-768"
-name: "Subtle Floral"
-products: Array(2)
-0: "Tops-Hibiscus-FloralKnitV-NeckTop"
-1: "Pants-Earnest-Black-MidToHighRisePants"
-length: 2
-
-each product has
-category: "dresses"
-in_stock: "true"
-is_international: true
-name: "Aadya"
-price: 2850
-tag_line: "Raw Silk Festive Fit & A-Line Dress"
-usd_price: 50
- */
