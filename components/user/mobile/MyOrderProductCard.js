22676df507920e957143c2e641c132f7e66a3890
Dhiraj Pandey <dhirjpndy@gmail.com>
2022-08-11 19:43:00 -0700

    order details get api

--- components/user/mobile/MyOrderProductCard.js
+++ components/user/mobile/MyOrderProductCard.js
@@ -5,36 +5,36 @@ import StarSVG from "./StarSVG";
 import Modal from "./MyOrdersModals"
 
 
-const MyOrderProductCard = ({product}) => {
+const MyOrderProductCard = ({product, itemIndex}) => {
     const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
     const [showModal, setShowModal] = useState(0)
 
-
+console.log('aaya')
     return (<>
         <div className={"flex flex-col bg-[#f7f7f7] mx-2 py-3"}>
             <div className={"flex justify-between gap-4"}>
                 <div className={"flex flex-col"}>
                     <Image
-                        src={WEBASSETS + product.img} alt="cart"
+                        src={WEBASSETS + "/assets/"+product.item[itemIndex].asset_id+"/thumb.jpg"} alt="cart"
                         width="129" height="208"/>
                     <span className={"flex flex-col items-center"}>
-                        <span> {product.title}</span>
-                        <span className={"flex gap-2 text-xs"}>Qty: {product.qty}<span>SIZE {product.size}</span></span>
+                        <span> {product.item.tagline}</span>
+                        <span className={"flex gap-2 text-xs"}>Qty: {product.item[itemIndex].qty}<span>SIZE {product.item[itemIndex].size}</span></span>
                     </span>
                 </div>
                 <div className={"flex flex-col"}>
-                    <span className={"block text-xs"}>ORDER # {product.orderID}</span>
+                    <span className={"block text-xs"}>ORDER # {product.order_id}</span>
                     <div>
                         <strong>ORDER PLACED</strong>
-                        <span className={"block mb-3"}>{product.date}</span>
+                        <span className={"block mb-3"}>{product.order_date_time}</span>
                         <strong>TOTAL AMOUNT:</strong>
-                        <span> {"Rs."}{product.price}</span>
+                        <span> {"Rs."}{product.payable}</span>
                     </div>
                     <div className={"flex flex-col my-2"}>
                         <strong> SHIP TO:</strong>
-                        <span className={"text-xs"}>A-66 10th Floor,Guru Nanak Pura</span>
-                        <span className={"text-xs"}>India,Kerala</span>
-                        <span className={"text-xs"}>Adimaly,682039</span>
+                        <span className={"text-xs"}>{product.delivery_address.address}</span>
+                        <span className={"text-xs"}>{product.delivery_address.country}, {product.delivery_address.state}</span>
+                        <span className={"text-xs"}>{product.delivery_address.city},{product.delivery_address.zip_code}</span>
                         <span
                             onClick={() => {
                                 setShowModal(1)
