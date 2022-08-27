022d647745e3ae6b74533445f6ff001d23538c02
Sambhav Sharma <sambhavsharma1999@gmail.com>
2022-08-27 13:17:10 +0530

    ---- Fixes

--- components/collections/Index.js
+++ components/collections/Index.js
@@ -39,18 +39,14 @@ const Index = () => {
                                             <Image src={WEBASSETS + item['img_path']} alt='collection' layout={`fill`} objectFit={`cover`}/>
                                         </span>
                                     </Link>
-                                    <p className='font-cursive italic leading-none text-2xl text-black/80 block text-center sentence'>{item['title']}</p>
-                                    <p className='text-[7.5px] text-[#b3aeab] text-center uppercase font-600 tracking-widest'>{item['subtitle'][currCurrency]}</p>
+                                    <p className='font-cursive italic leading-none text-3xl text-black/80 block text-center sentence'>{item['title']}</p>
+                                    <p className='text-[7.5px] text-[#b3aeab] text-center uppercase font-500 tracking-widest'>{item['subtitle'][currCurrency]}</p>
                                     <div className={`py-5 zoomInSwiper`}>
                                         <Swiper
                                             slidesPerView={2}
                                             spaceBetween={50}
                                             centeredSlides={true}
                                             loop={true}
-                                            autoplay={{
-                                                "delay": 2500,
-                                                "disableOnInteraction": false
-                                            }}
                                             navigation={true}
                                         >
                                             {item.products_imgs.map((product, i) => {
--- components/product-page/desktop-view/sub-sections/DetailsSection.js
+++ components/product-page/desktop-view/sub-sections/DetailsSection.js
@@ -1,16 +1,16 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import Image from "next/image";
 import AppWideContext from "../../../../store/AppWideContext";
 import appSettings from "../../../../store/appSettings";
 import Link from "next/link";
 import returnSizes from "../../../../helpers/returnSizes";
-import {addToCart} from "../../../../helpers/addTocart";
+import { addToCart } from "../../../../helpers/addTocart";
 import Toast from "../../../common/Toast";
 
-const DetailsSection = ({theme, data, selectedSize, setSelectedSize}) => {
+const DetailsSection = ({ theme, data, selectedSize, setSelectedSize }) => {
     const WEBASSETS = process.env.NEXT_PUBLIC_WEBASSETS;
 
-    const {dataStore, updateDataStore} = useContext(AppWideContext);
+    const { dataStore, updateDataStore } = useContext(AppWideContext);
     const currCurrency = dataStore.currCurrency;
     const currencyData = appSettings("currency_data");
     const currencySymbol = currencyData[currCurrency].curr_symbol;
@@ -34,7 +34,7 @@ const DetailsSection = ({theme, data, selectedSize, setSelectedSize}) => {
     let feature_icons = {}
 
     data.icon_assets.forEach((icon) => {
-        feature_icons = {...feature_icons, ...icon}
+        feature_icons = { ...feature_icons, ...icon }
     })
     let tags = Object.keys(data).map((key) => {
         if (key.startsWith('filter') && key !== "filter-collection" && key !== "filter-multi-color" && data[key]) {
@@ -60,7 +60,7 @@ const DetailsSection = ({theme, data, selectedSize, setSelectedSize}) => {
             sleeve_length: "",
             dress_length: ""
         }
-        addToCart(dataStore, updateDataStore, {cart: cart}).then(r => {
+        addToCart(dataStore, updateDataStore, { cart: cart }).then(r => {
             setToastMsg("Added to Cart")
             setShowToast(true)
         })
@@ -73,8 +73,8 @@ const DetailsSection = ({theme, data, selectedSize, setSelectedSize}) => {
             returnValue = <>
                 {returnValue}
                 <span key={"item" + index}
-                      className={"py-1.5 px-3.5 " + [(selectedSize == size) ? "border border-black text-black cursor-pointer" : "cursor-pointer border-t border-b border-transparent"]}
-                      onClick={() => setSelectedSize(size)}>
+                    className={"py-1.5 px-3.5 " + [(selectedSize == size) ? "border border-black text-black cursor-pointer" : "cursor-pointer border-t border-b border-transparent"]}
+                    onClick={() => setSelectedSize(size)}>
                     {size}
                 </span>
             </>
@@ -98,14 +98,14 @@ const DetailsSection = ({theme, data, selectedSize, setSelectedSize}) => {
                 </div>
                 <p className={"uppercase font-500 mb-4"}>size guide</p>
                 <button className={`border-2 border-${theme} w-10/12 hover:bg-black hover:text-white font-cursive italic font-600 pt-3 pb-1 text-2xl`}
-                        onClick={saveToCart}>i&lsquo;ll take it!
+                    onClick={saveToCart}>i&lsquo;ll take it!
                 </button>
             </div>
             <div className={"flex flex-wrap justify-center my-10"}>
                 {Object.keys(data.icons_fea).filter(key => data.icons_fea[key]).map((key, index) => (
                     <div className={"text-center flex flex-col justify-center items-center w-[33%]"} key={index}>
                         <div className={"relative aspect-square h-11 grayscale fill-black"}>
-                            <Image src={WEBASSETS + feature_icons[key]} alt='' layout={"fill"} objectFit={`cover`}/>
+                            <Image src={WEBASSETS + feature_icons[key]} alt='' layout={"fill"} objectFit={`cover`} />
                         </div>
                         <span className={"block"}>{key.replace("9_", "9-").replace(/_/g, " ")}</span>
                     </div>
@@ -118,33 +118,33 @@ const DetailsSection = ({theme, data, selectedSize, setSelectedSize}) => {
                     </Link>
                 })}
             </div>
-            <div className='mb-8'>
+            <div className='my-8'>
                 <div className={"flex items-end justify-start gap-10 font-600 mb-2"}>
-                    <p className={"uppercase"} onClick={() => setSelected(0)}>Description</p>
-                    <p className={"uppercase"} onClick={() => setSelected(1)}>details</p>
-                    <p className={"uppercase"} onClick={() => setSelected(2)}>fabric & care</p>
+                    <p className={"uppercase cursor-pointer"} onClick={() => setSelected(0)}>Description</p>
+                    <p className={"uppercase cursor-pointer"} onClick={() => setSelected(1)}>details</p>
+                    <p className={"uppercase cursor-pointer"} onClick={() => setSelected(2)}>fabric & care</p>
                 </div>
                 <ul className='list-disc text-sm font-500 pl-5'>
-                    {description.map((value, index) => <li className={""} key={index}>{value}</li>)}
+                    {description.map((value, index) => value.length > 0 && <li className={""} key={index}>{value}</li>)}
                 </ul>
             </div>
             <p className={"text-center font-500 text-sm mb-5"}>At SALT you can customize and tailor make your clothing</p>
             <div className={"flex mb-8"}>
                 <div className={"text-center flex flex-col justify-center items-center w-[33%]"}>
                     <div className={"relative aspect-square h-11"}>
-                        <Image src={WEBASSETS + "/assets/images/measure_yourself.svg"} alt='' layout={"fill"} objectFit={`cover`}/>
+                        <Image src={WEBASSETS + "/assets/images/measure_yourself.svg"} alt='' layout={"fill"} objectFit={`cover`} />
                     </div>
                     <p className={"font-600 text-sm"}>MADE TO MEASURE</p>
                 </div>
                 <div className={"text-center flex flex-col justify-center items-center w-[33%]"}>
                     <div className={"relative aspect-square h-11"}>
-                        <Image src={WEBASSETS + "/assets/images/free_shipping.svg"} alt='' layout={"fill"} objectFit={`cover`}/>
+                        <Image src={WEBASSETS + "/assets/images/free_shipping.svg"} alt='' layout={"fill"} objectFit={`cover`} />
                     </div>
                     <p className={"font-600 text-sm"}>FREE SHIPPING</p>
                 </div>
                 <div className={"text-center flex flex-col justify-center items-center w-[33%]"}>
                     <div className={"relative aspect-square h-11"}>
-                        <Image src={WEBASSETS + "/assets/images/perfect_fit.svg"} alt='' layout={"fill"} objectFit={`cover`}/>
+                        <Image src={WEBASSETS + "/assets/images/perfect_fit.svg"} alt='' layout={"fill"} objectFit={`cover`} />
                     </div>
                     <p className={"font-600 text-sm"}>PERFECT FIT GUARANTEE</p>
                 </div>
