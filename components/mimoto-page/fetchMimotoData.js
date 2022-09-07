143dbedaac80b6617971edcf169d2a459cb6aa71
Shubham Goel <shubham.goel1495@gmail.com>
2022-09-07 15:14:43 +0530

    bug fix

--- components/mimoto-page/fetchMimotoData.js
+++ components/mimoto-page/fetchMimotoData.js
@@ -4,7 +4,6 @@ export default async function fetchMimotoData(category){
     const callObject = await apiCall("getMimotoProducts", process.env.API_TOKEN, { name: category })
     return (callObject.hasOwnProperty("response")
         && callObject.hasOwnProperty("msg")
-        && callObject.msg === "Products Found"
     ) ? callObject.response : {}
 
 }
